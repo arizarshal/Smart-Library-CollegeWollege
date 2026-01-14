d19bc46eccfc7f6d7f8662b2d08664ead00b9e82
Ariz Arshal <arizarshal@gmail.com>
2026-01-14 16:12:30 +0530

    removed ESmodule from static frontennd

--- client/js/config.js
+++ client/js/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   API_BASE_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:5000"
-      : "https://smart-library-collegewollege.onrender.com/",
+      : "https://smart-library-collegewollege.onrender.com",
 };
